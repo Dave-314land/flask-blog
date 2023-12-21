b3930d52f0a2c94f1467774a88932e3775da4ecd
Dave-314land <dave.l.piland@gmail.com>
2023-12-21 15:52:44 -0500

    fixing typo

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY = 'dev', # change secret and upload from config file when deploying to prod
-        DATABASE = os.path.jon(app.instance_path, 'flaskr.sqlite'),
+        DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
     if test_config is None:

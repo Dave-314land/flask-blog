3e431c77b1c95a30e23836c204becd7d6efccb13
Dave-314land <dave.l.piland@gmail.com>
2023-12-21 15:51:08 -0500

    adding path for database instance

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,27 @@
+import os
+
 from flask import Flask
 
 def create_app(test_config=None):
     """Create and configure the app"""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev') # change this later to load from config file where secret is not exposed
+        SECRET_KEY = 'dev', # change secret and upload from config file when deploying to prod
+        DATABASE = os.path.jon(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent = True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    try:
+        # confirm the instance folder exists
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     @app.route('/hello')
     def hello():

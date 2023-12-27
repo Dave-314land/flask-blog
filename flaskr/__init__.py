2fae82f984eb7d9ee46362d43ec9de7cb4bf294b
Dave-314land <dave.l.piland@gmail.com>
2023-12-27 16:04:38 -0500

    added db initialization to app initialization

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -27,5 +27,8 @@ def create_app(test_config=None):
     def hello():
         """Hello world route for initial testing"""
         return "Hello World!"
+
+    from . import db
+    db.init_app(app)
     
     return app
